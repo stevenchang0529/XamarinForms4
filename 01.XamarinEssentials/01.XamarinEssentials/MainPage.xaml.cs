727c4f08901649d30738767b10e4c22e531af104
Steven <steven@tpisoftware.com>
2020-09-10 18:16:02 +0800

    add

---
+++ 01.XamarinEssentials/01.XamarinEssentials/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace _01.XamarinEssentials
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+
+            var current = Connectivity.NetworkAccess;
+            this.lblState.Text = current == NetworkAccess.Internet ? "有網路連線" : "無網路連線";
+
+
+
+
+        }
+
+        private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            var current = e.NetworkAccess;
+            this.lblState.Text = current == NetworkAccess.Internet ? "有網路連線" : "無網路連線";
+        }
+    }
+}
---
+++ 03.ShellTest/03.ShellTest/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace _03.ShellTest
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+           // Shell.SetTabBarIsVisible(this, false);
+
+        }
+
+    }
+}
